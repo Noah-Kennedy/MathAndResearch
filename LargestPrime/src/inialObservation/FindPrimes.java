53d1024447a8eefc17113785065143ec1612b9b0
Noah Kennedy <Nomaxx117@gmail.com>
2017-10-03 14:05:41 -0500

    Optimized primefinder
    Let's hope this works.

--- LargestPrime/src/inialObservation/FindPrimes.java
+++ LargestPrime/src/inialObservation/FindPrimes.java
@@ -1,26 +1,33 @@
 package inialObservation;
 
+import javax.swing.JOptionPane;
 
 public class FindPrimes {
 	
-	public static void simplePrime(int upto) {
+	public static void simplePrimeUpto(int upto) {
 		
 		//first loop, go through ints
 		for(int i = 1; i <= upto; i += 2) {
 			
 			boolean isPrime = true;
+			
 			//second loop, see if divisible by numbers less than it
-			for(int j = 2; j < i; j++) {
+			for(int j = 2; j < i / 2; j++) {
 				
-				if(i % j == 0) isPrime = false;
+				if(i % j == 0) {
+					isPrime = false;
+					break;
+				}
+				
 			}
 			
 			if(isPrime) System.out.println(i);
+			
 		}
 	}
 	
 	public static void main(String[] args) {
-		int upto = 50;
-		simplePrime(upto);
+		int upto = Integer.parseInt(JOptionPane.showInputDialog("Enter a number to go upto"));
+		simplePrimeUpto(upto);
 	}
 }
